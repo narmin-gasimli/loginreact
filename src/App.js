4ffe3503099edbad751d999e8cdafdfbc7c83b97
narmin-gasimli <nerminqasimli03@gmail.com>
2023-12-01 10:41:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Profil from "./components/Profil";
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const user = {
+    email: "nerminqasimli210@gmail.com",
+    name: "Narmin",
+    address: "Baku",
+    personalInfo: "I am 20",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Profil user={user} />
     </div>
   );
-}
 
-export default App;
+}
